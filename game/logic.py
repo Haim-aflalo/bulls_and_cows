62fca86c26ee4f122d73e0f61975ab0c4fb5ceb9
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-29 10:23:38 +0200

    function score_guess

---
+++ game/logic.py
@@ -0,0 +1,10 @@
+def score_guess(secret: str, guess: str) -> tuple[int, int]:
+    bulls = 0
+    cows = 0
+    for i in range(4):
+        if secret[i] == guess[i]:
+            bulls += 1
+        if secret[i] in guess and secret[i] != guess[i]:
+            cows += 1
+    return bulls,cows
+
