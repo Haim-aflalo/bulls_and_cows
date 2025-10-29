c91c3e60d4175ea4c744bf3746953b14b1813911
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-29 11:55:21 +0200

    print_status function

--- game/io.py
+++ game/io.py
@@ -1,3 +1,4 @@
+import logic
 def prompt_guess(length):
     while True:
         guess = input("enter your number: ")
@@ -8,6 +9,17 @@ def prompt_guess(length):
             print("the length of the number is not correct")
             continue
         return guess
+
+
+def print_status(state) -> None:
+
+        print("length:",state["length"],"\n"
+        "max_tries:",state["max_tries"],"\n"
+        "tries_used:",state["tries_used"],"\n"
+        "history:",state["history"],"\n"
+        "seen:",state["seen"])
+
+
 
 
 
