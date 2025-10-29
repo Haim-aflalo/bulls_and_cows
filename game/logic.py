3ae134413be91843899bc48d316dc9a86e3fd193
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-29 11:02:16 +0200

    function init_state

--- game/logic.py
+++ game/logic.py
@@ -10,3 +10,16 @@ def score_guess(secret: str, guess: str) -> tuple[int, int]:
 
 def is_wom(bulls,length):
     return bulls == length
+
+def init_state(secret: str, length: int, max_tries: int | None) -> dict:
+    game_state = {
+        "secret":secret,
+        "length": length,
+        "max_tries": max_tries,
+        "tries_used": int,
+        "history": list[tuple[str, int, int]],
+        "seen": set[str]
+    }
+    return  game_state
+
+
